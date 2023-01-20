809df3071a006c2f6c4b20583e7962cb61add61c
ryan.b <ryan.b@argodevops.co.uk>
2023-01-20 13:06:09 +0000

    fix: 🐛 added missing dependencies

--- release.config.js
+++ release.config.js
@@ -1,54 +1,50 @@
 /* eslint-disable no-template-curly-in-string */
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 const branches = [
-  "master",
-  "release/*",
-  {
-    name: "develop",
-    prerelease: true,
-  },
-  {
-    name: "alpha",
-    prerelease: true,
-  },
+    'master',
+    'release/*',
+    {
+        name: 'develop',
+        prerelease: true
+    },
+    {
+        name: 'alpha',
+        prerelease: true
+    }
 ];
 
-const commitAnalyzer = "@semantic-release/commit-analyzer";
-const releaseNotesGenerator = "@semantic-release/release-notes-generator";
-const changelog = "@semantic-release/changelog";
+const commitAnalyzer = '@semantic-release/commit-analyzer';
+const releaseNotesGenerator = '@semantic-release/release-notes-generator';
+const changelog = '@semantic-release/changelog';
 
 const docker = [
-  "@codedependant/semantic-release-docker",
-  {
-    dockerTags: [
-      "{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}",
-      "{{major}}-{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}",
-      "{{major}}.{{minor}}-{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}",
-      "{{version}}",
-    ],
-    dockerRegistry: "<ENTER DOCKER REGISTRY HERE>",
-    dockerArgs: {
-      NPM_AUTH_TOKEN: process.env.NPM_AUTH_TOKEN,
-    },
-    dockerProject: null,
-    dockerImage: pkg.name,
-  },
+    '@codedependant/semantic-release-docker',
+    {
+        dockerTags: [
+            '{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}',
+            '{{major}}-{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}',
+            '{{major}}.{{minor}}-{{#if prerelease.[0]}}{{prerelease.[0]}}{{else}}latest{{/if}}',
+            '{{version}}'
+        ],
+        dockerRegistry: '<ENTER DOCKER REGISTRY HERE>',
+        dockerProject: null,
+        dockerImage: pkg.name
+    }
 ];
 
-const git = "@semantic-release/git";
+const git = '@semantic-release/git';
 
-const teams = ["@argo/sematic-release-teams", { packageName: pkg.name }];
+const teams = ['@argodevops/semantic-release-teams', { packageName: pkg.name }];
 
 module.exports = {
-  branches,
-  plugins: [
-    commitAnalyzer,
-    releaseNotesGenerator,
-    changelog,
-    deprecate,
-    docker,
-    git,
-    teams,
-  ],
+    branches,
+    plugins: [
+        commitAnalyzer,
+        releaseNotesGenerator,
+        changelog,
+        docker,
+        git,
+        teams
+    ]
 };
