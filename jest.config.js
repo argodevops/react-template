99545bb357ca78c56d17edecb3efe7c86551aa6b
ryan.b <ryan.b@argodevops.co.uk>
2023-05-04 07:38:23 +0000

    fix: use parcel instead of webpack for quicker build times

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,14 @@ module.exports = {
     ],
     coverageDirectory: 'coverage',
     transform: {
-        '^.+\\.(js|jsx|mjs)$': 'babel-jest'
+        '^.+\\.(js|jsx)$': [
+            'babel-jest',
+            { configFile: './testConfig/babel.config.json' }
+        ]
     },
+    clearMocks: true,
     transformIgnorePatterns: [],
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+    setupFilesAfterEnv: ['<rootDir>/testConfig/setupTests.js'],
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': path.resolve('./__mocks__/styleMock.js'),
         '\\.(gif|ttf|eot|svg|png)$': path.resolve('./__mocks__/fileMock.js')
